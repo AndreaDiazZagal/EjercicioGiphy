e49feca6f6b97141584566e2715dd3fd73f7526b
AndreaDiazZagal <105683934+AndreaDiazZagal@users.noreply.github.com>
2023-01-20 05:59:01 -0600

    Add files via upload

---
+++ Practica1/Gif.js
@@ -0,0 +1,46 @@
+const form = document.querySelector("#form");
+const input =document.querySelector("#search");
+const paste = document.querySelector("#gifts");
+const END_POINT_SEARCH = "https://api.giphy.com/v1/gifs/search";
+const END_POINT_RANDOM = "https://api.giphy.com/v1/gifs/random";
+const MY_API_KEY = "YxK0LddBWYPtWzItpdgpgxfBO3L77BE6";  
+
+
+
+
+const showGifts = async (e) =>{
+    e.preventDefault();
+    console.log(input.value);
+
+    const URL = `${END_POINT_SEARCH}?api_key=${MY_API_KEY}&q=${input.value}`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data)
+    let template= '';
+    data.data.forEach(element => {
+        const {url} = element.images.original;
+        template+= `<img width="100px" height="100px" src="${url}"/>`
+    });
+
+    
+    paste.innerHTML= template;
+
+
+
+}
+//random will be when I change my page
+const random = async () =>{
+    let URL= `${END_POINT_RANDOM}?api_key=${MY_API_KEY}`;
+    const response = await fetch(URL);
+    const element = await response.json();
+    console.log(element)
+    const {url} = element.data.images.original;
+
+    paste.innerHTML= `<img width="100%" height="100p%" class= "p-3 m-1 img-thumbnail" src="${url}" alt="img-random"/>`;
+
+}
+form.addEventListener("submit", showGifts);
+document.addEventListener("DOMContentLoaded", random );
+
+
+
